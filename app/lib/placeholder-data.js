c0bb9f31c759fe5867c8ea031c08a7d32e783a27
Colum Kelly <columk1@users.noreply.github.com>
2024-02-16 23:57:53 -0800

    Add placeholder data

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: '1',
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john@example.com',
+    password: '12345',
+  },
+  {
+    id: '2',
+    firstName: 'Jane Doe',
+    lastName: 'Doe',
+    email: 'jane@example.com',
+    password: '12345',
+  },
+]
+
+export { users }
