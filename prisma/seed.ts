4ae6ac4347fdea48ca585de60f30234a08e4e2f5
Colum Kelly <columk1@users.noreply.github.com>
2024-02-17 22:27:30 -0800

    Remove old sql query from seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,19 +3,6 @@ import { users } from '@/app/lib/placeholder-data'
 import bcrypt from 'bcrypt'
 
 async function main() {
-  //     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  //     // Create the "users" table if it doesn't exist
-  //     const createTable = await client.sql`
-  //       CREATE TABLE IF NOT EXISTS users (
-  //         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  //         name VARCHAR(255) NOT NULL,
-  //         email TEXT NOT NULL UNIQUE,
-  //         password TEXT NOT NULL
-  //       );
-  //     `;
-
-  //     console.log(`Created "users" table`);
-
   // Insert data into the "users" table
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
