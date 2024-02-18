83dd76d378ac4a648d2efce6884cd50d9c5f3a41
Colum Kelly <columk1@users.noreply.github.com>
2024-02-17 21:13:02 -0800

    Add name field to input

--- app/ui/Input.tsx
+++ app/ui/Input.tsx
@@ -5,13 +5,14 @@ import clsx from 'clsx'
 interface InputProps {
   label: string
   id: string
+  name: string
   type?: string
   required?: boolean
   errors: any
   disabled?: boolean
 }
 
-const Input = ({ label, id, type, required, errors, disabled }: InputProps) => {
+const Input = ({ label, id, name, type, required, errors, disabled }: InputProps) => {
   return (
     <div>
       <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor={id}>
@@ -20,13 +21,15 @@ const Input = ({ label, id, type, required, errors, disabled }: InputProps) => {
       <div className='mt-2'>
         <input
           id={id}
+          name={name}
           type={type}
           autoComplete={id}
           disabled={disabled}
           required={required}
           className={clsx(
             `form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
-            errors[id] && 'focus: ring-rose-500'
+            errors[id] && 'focus: ring-rose-500',
+            disabled && 'opacity-50 cursor-default'
           )}
         />
       </div>
