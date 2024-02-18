e09deeaaeaa4c74f160c53ad48b511b9d069e049
Colum Kelly <columk1@users.noreply.github.com>
2024-02-17 21:12:22 -0800

    Fix multiple instances of Prisma in development

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -4,10 +4,21 @@ import { PrismaClient } from '@prisma/client'
 // import dotenv from 'dotenv'
 import ws from 'ws'
 // dotenv.config()
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
 neonConfig.webSocketConstructor = ws
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = `${process.env.DATABASE_URL}` // NextJS allows process.env to access .env.local
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
-const prisma = new PrismaClient({ adapter })
+
+const prismaClientSingleton = () => new PrismaClient({ adapter })
+
+const prisma = globalThis.prisma && prismaClientSingleton()
+
+// Prevent multiple instances of Prisma Client in development (hot reloading can create new instances)
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
 export default prisma
