97a0138d15fbb81d4662883ed82b8af305df47e3
Colum Kelly <columk1@users.noreply.github.com>
2024-02-21 13:43:22 -0800

    Create update api route for profile settings

---
+++ app/api/settings/route.ts
@@ -0,0 +1,28 @@
+import { getCurrentUser } from '@/app/lib/actions'
+import { NextResponse } from 'next/server'
+import prisma from '@/app/lib/prisma'
+
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser()
+    const body = await request.json()
+    const { name, image } = body
+
+    if (!currentUser?.id) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        image,
+        name,
+      },
+    })
+    return NextResponse.json(updatedUser)
+  } catch (error) {
+    console.log(error)
+    return new NextResponse('Internal Error', { status: 500 })
+  }
+}
