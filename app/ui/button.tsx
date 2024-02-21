12d69d2f2edf255cb754cf1d9f603371f5cd5e2f
Colum Kelly <columk1@users.noreply.github.com>
2024-02-20 22:36:34 -0800

    Update Button component, add style props

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,19 +1,43 @@
 import clsx from 'clsx'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode
+  type?: 'button' | 'submit' | 'reset' | undefined
+  fullWidth?: boolean
+  onClick?: () => void
+  secondary?: boolean
+  danger?: boolean
+  disabled?: boolean
+  children?: React.ReactNode
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+const Button = ({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+  className,
+}: ButtonProps) => {
   return (
     <button
-      {...rest}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
-        className
+        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50 ' +
+          className,
+        disabled && 'opacity-50 cursor-default',
+        fullWidth && 'w-full',
+        secondary ? 'text-gray-900' : 'text-white',
+        danger && 'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
+        !secondary && !danger && 'bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600'
       )}
     >
       {children}
     </button>
   )
 }
+
+export default Button
