8cf6fce26f38a4e28f4f0a019613eb2e9dae3668
Colum Kelly <columk1@users.noreply.github.com>
2024-02-21 00:12:45 -0800

    Fix Input component to make errors array optional

--- app/ui/Input.tsx
+++ app/ui/Input.tsx
@@ -8,7 +8,7 @@ interface InputProps {
   name: string
   type?: string
   required?: boolean
-  errors: any
+  errors?: any
   disabled?: boolean
 }
 
@@ -28,7 +28,7 @@ const Input = ({ label, id, name, type, required, errors, disabled }: InputProps
           required={required}
           className={clsx(
             `form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
-            errors[id] && 'focus: ring-rose-500',
+            errors && errors[id] && 'focus: ring-rose-500',
             disabled && 'opacity-50 cursor-default'
           )}
         />
