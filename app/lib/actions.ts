0f2ddce975ebe1362cc573656f4c15c0a5b40792
Colum Kelly <columk1@users.noreply.github.com>
2024-02-24 16:12:03 -0800

    Separate exports for authOptions and auth api route handler

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,7 @@
 import prisma from '@/app/lib/prisma'
 import { getServerSession } from 'next-auth'
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/app/lib/auth'
 
 const getSession = async () => await getServerSession(authOptions)
 
