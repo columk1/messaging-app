db0232a7c43eeacb91640f239daf107c41778049
Colum Kelly <columk1@users.noreply.github.com>
2024-02-27 16:05:08 -0800

    Redirect / page to users

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import Image from 'next/image'
-import Logo from '@/app/ui/svg/Logo'
-import AuthForm from '@/app/ui/AuthForm'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <main className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8'>
-      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-        <Logo className='h-12 w-12 mx-auto stroke-emerald-500' />
-        <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
-          Sign in to your account
-        </h2>
-      </div>
-      <AuthForm />
-    </main>
-  )
+  redirect('/users')
 }
