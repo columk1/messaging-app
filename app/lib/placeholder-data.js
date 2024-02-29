988a81c036da2799082b83ec1f1bd433320baee8
Colum Kelly <columk1@users.noreply.github.com>
2024-02-29 13:51:11 -0800

    Remove unused fields, add to seed data

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -9,6 +9,16 @@ const users = [
     email: 'jane@example.com',
     password: '12345',
   },
+  {
+    name: 'Jim Doe',
+    email: 'jim@example.com',
+    password: '12345',
+  },
+  {
+    name: 'Jill Doe',
+    email: 'jill@example.com',
+    password: '12345',
+  },
 ]
 
 export { users }
