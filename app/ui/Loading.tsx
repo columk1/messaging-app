607cdd74f1420a8174011e25a7b3c2fa840f1c86
Colum Kelly <columk1@users.noreply.github.com>
2024-03-08 11:01:02 -0800

    Fix auto-scroll error due to position fixed

--- app/ui/Loading.tsx
+++ app/ui/Loading.tsx
@@ -3,12 +3,12 @@
 import React, { Fragment } from 'react'
 import Spinner from './Spinner'
 
-const LoadingModal = () => {
+const Loading = () => {
   return (
-    <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-purple-3 bg-opacity-50 backdrop-blur-sm'>
+    <div className='w-full h-full flex justify-center items-center z-50 bg-purple-3 bg-opacity-50 backdrop-blur-sm'>
       <Spinner />
     </div>
   )
 }
 
-export default LoadingModal
+export default Loading
