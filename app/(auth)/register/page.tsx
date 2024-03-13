1dc1ed856a91f598773887e7f652713385ae15af
Colum Kelly <columk1@users.noreply.github.com>
2024-03-12 23:49:00 -0700

    Customize auth pages

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,16 @@
 import AuthForm from '@/app/ui/AuthForm'
+import Logo from '@/app/ui/svg/Logo'
 
 export default function Home() {
-  return <AuthForm formType='REGISTER' />
+  return (
+    <>
+      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+        <Logo className='h-12 w-12 mx-auto stroke-violet-400' />
+        <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-200'>
+          Create a new account
+        </h2>
+      </div>
+      <AuthForm formType='REGISTER' />
+    </>
+  )
 }
