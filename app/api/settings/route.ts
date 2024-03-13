77a160686ae7757a81c5b9d87f04ab4ddd2a7c5d
Colum Kelly <columk1@users.noreply.github.com>
2024-03-13 13:55:12 -0700

    Add validation to settings update

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -2,15 +2,34 @@ import { getCurrentUser } from '@/app/lib/actions'
 import { NextResponse } from 'next/server'
 import prisma from '@/app/lib/prisma'
 
+import { z } from 'zod'
+
+const SettingsSchema = z.object({
+  name: z.string().optional(),
+  image: z.string().optional(),
+})
+
 export async function POST(request: Request) {
   try {
-    const currentUser = await getCurrentUser()
     const body = await request.json()
-    const { name, image } = body
+    const currentUser = await getCurrentUser()
 
     if (!currentUser?.id) {
       return new NextResponse('Unauthorized', { status: 401 })
     }
+
+    if (!body.name && !body.image) {
+      return new NextResponse('Missing data', { status: 400 })
+    }
+
+    const validatedFields = SettingsSchema.safeParse(body)
+
+    if (!validatedFields.success) {
+      return new NextResponse('Invalid data', { status: 400 })
+    }
+
+    const { name, image } = validatedFields.data
+
     const updatedUser = await prisma.user.update({
       where: {
         id: currentUser.id,
