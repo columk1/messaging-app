0f1c37e251699c2f876ad979271dd11ac5bf0c09
Colum Kelly <columk1@users.noreply.github.com>
2024-03-15 00:16:03 -0700

    Add custom type extensions for next-auth

--- app/next-auth.d.ts
+++ app/next-auth.d.ts
@@ -3,9 +3,16 @@ import 'next-auth'
 declare module 'next-auth' {
   interface User {
     id: string
+    username: string
   }
 
   interface Session {
     user: User
+  }
+
+  interface Profile {
+    login: string
+    given_name: string
+    family_name: string
   }
 }
