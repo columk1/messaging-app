48990b03ab026753417c08ac67373a7783b2006e
Colum Kelly <columk1@users.noreply.github.com>
2024-03-15 23:33:15 -0700

    Add aria-label and autofill styling

--- app/ui/Input.tsx
+++ app/ui/Input.tsx
@@ -6,8 +6,10 @@ import clsx from 'clsx'
 interface InputProps {
   type?: string
   label: string
+  ariaLabel?: string
   id: string
   name: string
+  value?: string
   defaultValue?: string
   placeholder?: string
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
@@ -22,8 +24,10 @@ interface InputProps {
 const Input = ({
   type,
   label,
+  ariaLabel,
   id,
   name,
+  value,
   defaultValue,
   placeholder,
   onChange,
@@ -36,7 +40,10 @@ const Input = ({
 }: InputProps) => {
   return (
     <div>
-      <label className='block text-sm font-medium leading-6 text-gray-200' htmlFor={id}>
+      <label
+        className={clsx('text-sm font-medium leading-6 text-gray-200', ariaLabel && 'hidden')}
+        htmlFor={id}
+      >
         {label}
       </label>
       <div className='mt-2'>
@@ -47,13 +54,15 @@ const Input = ({
           defaultValue={defaultValue}
           placeholder={placeholder}
           onChange={onChange}
+          value={value}
           autoComplete={id}
           disabled={disabled}
           required={required}
           minLength={min}
           maxLength={max}
+          aria-label={ariaLabel}
           className={clsx(
-            `peer block w-full rounded-md border-0 py-1.5 px-3 text-gray-200 bg-purple-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-300 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-rose-500`,
+            `peer block w-full rounded-md border-0 py-1.5 px-3 text-gray-200 bg-purple-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-350 focus:ring-2 focus:ring-inset focus:ring-violet-300 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:ring-rose-500 autofill:shadow-[inset_0_0_0px_1000px_#c4b5fd]`,
             errors && errors[id] && 'focus:ring-rose-500',
             disabled && 'opacity-50 cursor-default'
           )}
