aab769c2ce0526b62dcd697c4c172cb4a1267426
Colum Kelly <columk1@users.noreply.github.com>
2024-03-21 01:07:29 -0700

    Update to seed users, conversations, messages, seen status, contacts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import prisma from '@/app/lib/prisma'
-import { users } from '@/app/lib/placeholder-data'
+import { users, conversations } from '@/app/lib/placeholder-data'
 import bcrypt from 'bcryptjs'
 
 async function main() {
@@ -11,18 +11,69 @@ async function main() {
         where: { email: user.email },
         update: {},
         create: {
+          id: user.id,
           name: user.name,
           email: user.email,
           username: user.username,
           password: hashedPassword,
+          image: user.image,
           createdAt: new Date(),
         },
       })
     })
   )
 
+  // Connect contacts to demo account now that all users exist
+  await prisma.user.update({
+    where: {
+      id: users[0].id,
+    },
+    data: {
+      contacts: {
+        connect: users[0].contacts?.map((contact) => ({ id: contact })),
+      },
+    },
+  })
+
+  const insertedConversations = await Promise.all(
+    conversations.map(async (conversation) => {
+      const insertedConversation = await prisma.conversation.create({
+        data: {
+          name: conversation.name || null,
+          isGroup: conversation.isGroup || null,
+          users: {
+            connect: conversation.userIds.map((userId) => ({ id: userId })),
+          },
+          userIds: conversation.userIds,
+          createdAt: new Date(),
+        },
+      })
+      for (const message of conversation.messages) {
+        const newMessage = await prisma.message.create({
+          data: {
+            body: message.body,
+            image: message.image,
+            sender: { connect: { id: message.senderId } },
+            conversation: { connect: { id: insertedConversation.id } },
+            createdAt: new Date(),
+          },
+        })
+        if (message.seenId) {
+          console.log('seenId', message.seenId)
+          await prisma.message.update({
+            where: { id: newMessage.id },
+            data: {
+              seen: { connect: { id: message.seenId } },
+            },
+          })
+        }
+      }
+    })
+  )
+
   console.log(`Seeded ${insertedUsers.length} users`)
   console.log(insertedUsers[0])
+  console.log(`Seeded ${insertedConversations.length} conversations`)
 }
 
 main()
